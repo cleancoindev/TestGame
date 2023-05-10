e9b9b1fe2ef70a843a2dd448019f9d6be40b7ce8
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-05-10 10:34:38 -1000

    Update Rewards.tsx

--- application/components/Rewards.tsx
+++ application/components/Rewards.tsx
@@ -1,77 +1,50 @@
-import React from "react";
-import {
-  ThirdwebNftMedia,
-  useAddress,
-  useContractRead,
-  useMetadata,
-  useTokenBalance,
-  Web3Button,
-} from "@thirdweb-dev/react";
-import { SmartContract, Token } from "@thirdweb-dev/sdk";
-import { ethers } from "ethers";
+import React from 'react';
+import { useState } from 'react';
+import { useWeb3 } from '@solana/web3.js';
+import { useRouter } from 'next/router';
 
-import styles from "../styles/Home.module.css";
-import ApproxRewards from "./ApproxRewards";
-import { MINING_CONTRACT_ADDRESS } from "../const/contractAddresses";
+import ThirdwebNftMedia from '@thirdweb/nft-media';
 
-type Props = {
-  miningContract: SmartContract<any>;
-  tokenContract: Token;
-};
+import styles from './Rewards.module.css';
 
-/**
- * This component shows the:
- * - Metadata of the token itself (mainly care about image)
- * - The amount this wallet holds of this wallet
- * - The amount this user can claim from the mining contract
- */
-export default function Rewards({ miningContract, tokenContract }: Props) {
-  const address = useAddress();
+const Rewards = () => {
+  const { publicKey } = useRouter();
+  const { connection } = useWeb3();
+  const [tokenMetadata, setTokenMetadata] = useState();
 
-  const { data: tokenMetadata } = useMetadata(tokenContract);
-  const { data: currentBalance } = useTokenBalance(tokenContract, address);
-  const { data: unclaimedAmount } = useContractRead(
-    miningContract,
-    "calculateRewards",
-    [address]
-  );
+  const handleClick = () => {
+    // Get the token metadata
+    const { id } = useRouter().query;
+    connection.request({
+      method: 'get',
+      path: `/metadata/${id}`,
+    }).then((response) => {
+      setTokenMetadata(response.json());
+    });
+  };
 
-return (
-  <div
-    style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-  >
-    <div>
-      <p>
-        Your <b>Gold Gems</b>
-      </p>
-
-      {tokenMetadata && (
-        <ThirdwebNftMedia
-          metadata={tokenMetadata}
-          height={"48"}
-        />
-      )}
-    </div>
-  );
-      <div>
-      <p className={styles.noGapBottom}>
-        Balance: <b>{currentBalance?.displayValue}</b>
-      </p>
-      <p>
-        Unclaimed:{" "}
-        <b>{unclaimedAmount && ethers.utils.formatUnits(unclaimedAmount)}</b>
-      </p>
-
-      <ApproxRewards miningContract={miningContract} />
-
-      <div className={styles.smallMargin}>
-        <Web3Button
-          contractAddress={MINING_CONTRACT_ADDRESS}
-          action={(contract) => contract.call("claim")}
-        >
-          Claim
-        </Web3Button>
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        Your Rewards
+      </div>
+      <div className={styles.content}>
+        <p>
+          You have <b>{tokenMetadata?.totalSupply}</b> Gold Gems
+        </p>
+        <button onClick={handleClick}>
+          Stake
+        </button>
+        {tokenMetadata && (
+          <ThirdwebNftMedia
+            metadata={tokenMetadata}
+            height={"48"}
+          />
+        )}
+        &rbrace;
       </div>
     </div>
   );
-}
+};
+
+export default Rewards;
